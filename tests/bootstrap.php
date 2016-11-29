2552f11341ceb47b1a3f0839c30f3bdc601cc42d
john arroyo <john.arroyo@gmail.com>
2016-11-29 13:00:12 -0800

    fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,15 @@
 <?php
-echo dirname(dirname(dirname(dirname(__DIR__)))).'/bootstrap.php';
-require_once dirname(dirname(dirname(dirname(__DIR__)))).'/bootstrap.php'; // boot up Erdiko
+// boot up Erdiko
+
+// This is for standard installations
+$bootstrap = dirname(dirname(dirname(dirname(__DIR__)))).'/app/bootstrap.php';
+
+// This is for Docker (works within docker and Travis CI)
+if(!file_exists($bootstrap))
+	$bootstrap = '/code/app/bootstrap.php';
+
+// This is for relative local dev
+if(!file_exists($bootstrap))
+	$bootstrap = dirname(dirname(__DIR__)).'/user-admin/app/bootstrap.php';
+
+require_once $bootstrap;
