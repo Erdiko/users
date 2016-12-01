8402573f247b4a09a06a0341019b94bcfe962491
Julian Diaz <julian@arroyolabs.com>
2016-12-01 15:02:21 -0300

    fixed overwrite original file.

--- sql/create_user.sql
+++ sql/create_user.sql
@@ -1,1 +1,22 @@
-ALTER TABLE  `users` CHANGE  `role`  `role` INT( 11 ) NOT NULL ;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `role` varchar(16) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `gateway_customer_id` varchar(16) DEFAULT NULL,
+  `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
