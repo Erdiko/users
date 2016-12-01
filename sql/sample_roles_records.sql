8eea4ab5e11e24ba6c5c88d18099d2cb6a0d8deb
Julian Diaz <julian@arroyolabs.com>
2016-12-01 15:00:34 -0300

    sample roles records.

--- sql/sample_roles_records.sql
+++ sql/sample_roles_records.sql
@@ -1,4 +1,4 @@
 INSERT INTO `roles` (`id`, `name`, `active`, `created`, `updated`)
 VALUES
-	(1, 'admin', '1', '2016-10-20 18:08:53', NULL),
-	(2, 'anonymous', '1', '2016-10-20 18:09:05', NULL)
+	(1, 'anonymous', '1', '2016-10-20 18:08:53', NULL),
+	(2, 'admin', '1', '2016-10-20 18:09:05', NULL);
--- sql/sample_users_records.sql
+++ sql/sample_users_records.sql
@@ -1,4 +1,4 @@
-INSERT INTO `roles` (`id`, `name`, `active`, `created`, `updated`)
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `name`, `gateway_customer_id`, `last_login`, `created_at`, `updated_at`)
 VALUES
-	(1, 'admin', '1', '2016-10-20 18:08:53', NULL),
-	(2, 'anonymous', '1', '2016-10-20 18:09:05', NULL)
+	(1, 'bar@mail.com', 'b05ebef6da920b9f6cffa2735b6d8f93', '1', 'Bar', NULL, NULL, '2016-10-20 18:08:53', NULL),
+	(2, 'foo@mail.com', 'b05ebef6da920b9f6cffa2735b6d8f93', '2', 'Foo', NULL, NULL, '2016-10-20 18:09:05', NULL);
