cc74c9f3bce29f145dddf71c8ac0635d0296cb34
Andy Armstrong <andyarmstro@gmail.com>
2017-02-17 17:09:59 -0800

    moved assets ; added sql structure dump

---
+++ sql/dumps/user-admin.sql
@@ -0,0 +1,65 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : User Admin
+ Source Server Type    : MySQL
+ Source Server Version : 50713
+ Source Host           : localhost
+ Source Database       : user-admin
+
+ Target Server Type    : MySQL
+ Target Server Version : 50713
+ File Encoding         : utf-8
+
+ Date: 02/17/2017 17:05:26 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `active` int(1) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Table structure for `user_event_log`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_event_log`;
+CREATE TABLE `user_event_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `event_log` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `event_data` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `role` varchar(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `gateway_customer_id` varchar(16) DEFAULT NULL,
+  `last_login` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=192 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
